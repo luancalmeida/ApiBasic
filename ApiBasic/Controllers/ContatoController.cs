97192b88ca32fe875f60056634a2fcd22b0b8758
luancalmeida <aluan64@gmail.com>
2024-06-05 18:38:58 -0300

    Realizado o endpoint do delete do crud

--- ApiBasic/Controllers/ContatoController.cs
+++ ApiBasic/Controllers/ContatoController.cs
@@ -57,8 +57,21 @@ namespace ApiBasic.Controllers
 
             return Ok(contatoBanco);
 
-        }  
+        }
 
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id) 
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+            if (contatoBanco == null)
+                return NotFound();
+
+            _context.Contatos.Remove(contatoBanco);
+            _context.SaveChanges();
+
+            return NoContent();
+            
+        }
 
     }
 }
